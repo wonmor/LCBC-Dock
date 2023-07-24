c756fcfe26677fbe5b9cd365b65e2f7b98190c8d
John Seong <wonmor@gmail.com>
2023-07-24 17:14:24 +0900

    UPDATED FAST API

--- api/index.py
+++ api/index.py
@@ -1,7 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import httpx
+import json
+from typing import List
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+# CORS middleware to allow requests from the frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Update with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+async def fetch_search_results(search_term: str) -> List[str]:
+    url = "https://search.rcsb.org/rcsbsearch/v2/query"
+    json_data = {
+        "query": {
+            "type": "terminal",
+            "service": "full_text",
+            "parameters": {
+                "value": search_term,
+            },
+        },
+        "return_type": "entry",
+    }
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, json=json_data)
+            response.raise_for_status()
+            data = response.json()
+
+            print(response.json())
+
+        if "result_set" in data:
+            return [result["identifier"] for result in data["result_set"]]
+        return []
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/search/{search_term}")
+async def search_proteins(search_term: str):
+    try:
+        return await fetch_search_results(search_term)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- app/docking/start/page.tsx
+++ app/docking/start/page.tsx
@@ -1,111 +1,83 @@
 "use client";
 
-import Link from "next/link";
 import AsyncSelect from "react-select/async";
 import axios from "axios";
+import Link from "next/link";
 
-import { MolStarWrapper } from "@/app/wrapper";
 import { useState } from "react";
+import { debounce } from "lodash";
 
 export default function Docking() {
   const [selectedOption, setSelectedOption] = useState(null);
   const [options, setOptions] = useState([]);
 
-  // Function to get PDB data and display it
-  const getPdbData = async (pdbId: string) => {
+  // Debounce the API call with a delay of 500ms
+  const debouncedSearch = debounce(async (inputValue) => {
     try {
-      const response = await axios.get(
-        `https://files.rcsb.org/view/${pdbId}.pdb`
-      );
-    } catch (error) {
-      console.error(`Error fetching PDB data: ${error}`);
-    }
-  };
-
-  const loadOptions = async (inputValue: string) => {
-    try {
-      if (inputValue && inputValue.trim() !== "") {
-        const response = await axios.post(
-          "https://search.rcsb.org/rcsbsearch/v1/query",
-          {
-            query: {
-              type: "terminal",
-              service: "text",
-              parameters: {
-                value: inputValue.trim(),
-              },
-            },
-            request_options: {
-              return_all_hits: true,
-            },
-            request_info: {
-              do_sequence: false,
-              do_taxonomy: false,
-            },
-          }
-        );
-
-        // Transform the response to get the options for the Select component
-        return response.data.result_set.map((result: any) => ({
-          value: result.identifier,
-          label: result.identifier,
-        }));
-      }
+      const response = await axios.get(`http://localhost:8000/search/${inputValue}`);
+      console.log(response.data)
+      setOptions(response.data);
     } catch (error) {
       console.error(`Error searching PDB: ${error}`);
     }
+  }, 500);
+
+  const loadOptions = (inputValue) => {
+    if (inputValue && inputValue.trim() !== "") {
+      debouncedSearch(inputValue.trim());
+    } else {
+      setOptions([]); // Clear options when search input is empty
+    }
   };
 
-  const handleChange = (selectedOption: any) => {
+  const handleChange = (selectedOption) => {
     setSelectedOption(selectedOption);
-    getPdbData(selectedOption.value);
+    // What to do on selection...
   };
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24 bg-gradient-to-br from-gray-900 to-black text-white">
-      {/* SVG representation of protein-ligand docking */}
-      <Link href="/">
-        <svg
-          width="300"
-          height="200"
-          viewBox="0 0 300 200"
-          xmlns="http://www.w3.org/2000/svg"
-          className="mb-8 cursor-pointer"
-        >
-          {/* Protein */}
-          <rect x="20" y="20" width="120" height="30" fill="#FF5733" />
-          <rect x="40" y="60" width="80" height="30" fill="#FF5733" />
-          <rect x="60" y="100" width="40" height="30" fill="#FF5733" />
-          {/* Ligand */}
-          <rect x="200" y="70" width="60" height="30" fill="#66BB6A" />
-          <rect x="230" y="110" width="20" height="30" fill="#66BB6A" />
-          {/* Bond */}
-          <line
-            x1="120"
-            y1="35"
-            x2="200"
-            y2="85"
-            stroke="white"
-            strokeWidth="2"
-          />
-        </svg>
-      </Link>
+    {/* SVG representation of protein-ligand docking */}
+    <Link href="/">
+      <svg
+        width="300"
+        height="200"
+        viewBox="0 0 300 200"
+        xmlns="http://www.w3.org/2000/svg"
+        className="mb-8 cursor-pointer"
+      >
+        {/* Protein */}
+        <rect x="20" y="20" width="120" height="30" fill="#FF5733" />
+        <rect x="40" y="60" width="80" height="30" fill="#FF5733" />
+        <rect x="60" y="100" width="40" height="30" fill="#FF5733" />
+        {/* Ligand */}
+        <rect x="200" y="70" width="60" height="30" fill="#66BB6A" />
+        <rect x="230" y="110" width="20" height="30" fill="#66BB6A" />
+        {/* Bond */}
+        <line
+          x1="120"
+          y1="35"
+          x2="200"
+          y2="85"
+          stroke="white"
+          strokeWidth="2"
+        />
+      </svg>
+    </Link>
 
-      {/* Title */}
-      <div className="text-center">
-        <h1 className="text-6xl font-thin mb-6">PROTEIN GOES HERE</h1>
-      </div>
+    {/* Title */}
+    <div className="text-center">
+      <h1 className="text-6xl font-thin mb-6">PROTEIN GOES HERE</h1>
+    </div>
 
-      {/* Search Bar */}
       <AsyncSelect
         cacheOptions
         defaultOptions
         loadOptions={loadOptions}
         onInputChange={(inputValue) => inputValue.trim()}
         onChange={handleChange}
+        options={options.map((result) => ({ value: result, label: result }))} // Map data for the AsyncSelect component
       />
-
-      <MolStarWrapper />
     </main>
   );
 }
