e31750cd42822f6bafc9039dddd85c99513c1c96
John Seong <wonmor@gmail.com>
2023-07-25 17:56:12 +0900

    Update page.tsx

--- app/docking/start/page.tsx
+++ app/docking/start/page.tsx
@@ -7,6 +7,8 @@ import axios from "axios";
 import AsyncSelect from "react-select/async";
 import ProgressBar from "@/app/progressBar";
 
+// HOW TO REMOVE H2O FROM A PDB FILE...
+
 const Docking: FC = () => {
   const [proteinState, setProteinState] = useState(null) as any;
 
