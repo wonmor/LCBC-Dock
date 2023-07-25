7104863982285c130a4b6de09a429cbb84310a29
John Seong <wonmor@gmail.com>
2023-07-25 21:15:21 +0900

    UPDATED TO DISPLAY PDB

--- app/docking/marinate/page.tsx
+++ app/docking/marinate/page.tsx
@@ -1,16 +1,19 @@
-"use client";
+"use client"
 
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { MolStarWrapper } from "@/app/wrapper";
-
 import axios from "axios";
 import ProgressBar from "@/app/progressBar";
+import { RotatingLines } from 'react-loader-spinner'; //Importing the loader/spinner library
 
 const Marinate: FC = () => {
   const searchParams = useSearchParams();
 
   const proteinState = searchParams.get("proteinState") ?? null;
+
+  const [proteinData, setProteinData] = useState(null); //State to store the fetched PDB file data
+  const [loading, setLoading] = useState(false); //State to control the spinner
 
   const scrolltoHash = function (element_id: string) {
     const element = document.getElementById(element_id);
@@ -50,6 +53,27 @@ const Marinate: FC = () => {
     }, 500);
   };
 
+  // useEffect to fetch the PDB file data once the component is loaded
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true); //Set loading to true to show the spinner
+      try {
+        const response = await axios.get(
+          `https://files.rcsb.org/download/${proteinState}.pdb`
+        );
+        setProteinData(response.data);
+      } catch (error) {
+        console.error(`Error fetching PDB file: ${error}`);
+      } finally {
+        setLoading(false); //Hide the spinner after data is fetched
+      }
+    };
+
+    if (proteinState) {
+      fetchData();
+    }
+  }, [proteinState]);
+
   return (
     <main className="pb-40">
       {proteinState && (
@@ -63,7 +87,15 @@ const Marinate: FC = () => {
           </div>
 
           <div id="wrapper">
-            <MolStarWrapper value={proteinState} />
+            {/*Show spinner while loading the data*/}
+            {loading && <RotatingLines />}
+            
+            {/*Display PDB file data once it is fetched*/}
+            {!loading && proteinData && 
+              <div className="pdb-data">
+                <pre>{proteinData}</pre>
+              </div>
+            }
           </div>
         </>
       )}
